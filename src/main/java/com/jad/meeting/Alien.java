eea206055d699abb1d1fd6e52c220a7d6b4b4235
Jean-Aymeric Diet <jeanaymeric@gmail.com>
2023-05-31 10:32:20 +0200

    add exercice with an alien who is meeting meetable

---
+++ src/main/java/com/jad/meeting/Alien.java
@@ -0,0 +1,9 @@
+package com.jad.meeting;
+
+public class Alien {
+    public void meet(final Meetable meetable) {
+        // pour une compagnie : il prend la place du PDG
+        // pour un ami : il en fait son sidekick
+        // pour un amant : il le mange
+    }
+}
---
+++ src/test/java/com/jad/meeting/AlienTest.java
@@ -0,0 +1,54 @@
+package com.jad.meeting;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class AlienTest {
+    private static final int NB_EMPLOYEES = 10;
+    private static final Company COMPANY = new Company("Label expected");
+    private static final Friend FRIEND = new Friend("FriendLastName", "FriendFirstName", "FriendSurname");
+    private static final Paramour PARAMOUR = new Paramour("ParamourLastName", "ParamourFirstName");
+    private final ByteArrayOutputStream outputStreamCaptor = new ByteArrayOutputStream();
+
+    @BeforeAll
+    static void init() {
+        for (int i = 0; i < AlienTest.NB_EMPLOYEES; i++) {
+            AlienTest.COMPANY.addEmployees(new Person("LastName" + i, "FirstName" + i));
+        }
+    }
+
+    @BeforeEach
+    public void setUp() {
+        System.setOut(new PrintStream(this.outputStreamCaptor));
+    }
+
+    @Test
+    void meetFriend() {
+        final Person person = new Person("LastName", "FirstName");
+        person.meet((Meetable) AlienTest.FRIEND);
+        assertEquals(AlienTest.FRIEND.getSurName() + " devient mon sidekick.",
+                this.outputStreamCaptor.toString().trim());
+    }
+
+    @Test
+    void meetParamour() {
+        final Person person = new Person("LastName", "FirstName");
+        person.meet((Meetable) AlienTest.PARAMOUR);
+        assertEquals("Je dévore " + AlienTest.PARAMOUR.getFirstName(), this.outputStreamCaptor.toString().trim());
+    }
+
+    @Test
+    void meetCompany() {
+        final Person person = new Person("LastName", "FirstName");
+        person.meet((Meetable) AlienTest.COMPANY);
+        assertEquals(
+                "Je prends la place du pdg de la boite " + AlienTest.COMPANY.getLabel(),
+                this.outputStreamCaptor.toString().trim());
+    }
+}
